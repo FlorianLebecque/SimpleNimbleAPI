4ddd1ac0d8c9c75c5386c8e085538458bd2e63e8
Florian <Florianlebecque@hotmail.com>
2022-03-28 16:15:11 +0200

    Add register route

--- routers/rt-users.js
+++ routers/rt-users.js
@@ -1,9 +1,42 @@
 const Express = require("express");
 const userRouter = Express.Router();
 
+const ctrl = global.ctrl;
+
 userRouter.get("/",(req,res)=>{
     res.json("hello");
 })
+
+
+userRouter.post("/register",async (req,res)=>{
+
+    let user_form = req.body;
+
+    
+    if(!ctrl.CheckObj(user_form,["name","email","password"])){
+        res.status(400).json("Bad request, incomplete form");
+        return;
+    }
+
+    
+    try {
+        let result = await ctrl.Register(user_form);
+
+        if(result){
+            res.json(result);
+            return
+        }else{
+            res.status(409).json("Conflict, email already taken");
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json("Internal error");
+    }
+
+    
+});
+
 
 let expt = {
     path:"user",
