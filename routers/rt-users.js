9640e5e25b5a77984ccbda0660cde5c61fb8c474
Florian Lebecque <=>
2022-04-28 22:24:28 +0200

    Implemented route for follow

--- routers/rt-users.js
+++ routers/rt-users.js
@@ -6,19 +6,28 @@ const ctrl = require("../controler/Controler");
 userRouter.get("/",(req,res)=>{
     //return a list of all user
 
-
+    res.json("hello world");
 
 })
 
-userRouter.get("/:search",(req,res)=>{
+userRouter.get("/s/:search",async (req,res)=>{
     //return a list of all user
 
     if(req.params.search){
+        let find_form = {
+            name : req.params.search
+        };
 
-    }else
+        let response = await ctrl.FindUser(find_form);
+
+        if(response){
+            res.json(response);
+        }
+    }else{
+        res.status(400).json("No parameters");
+    }
     
-
-})
+});
 
 userRouter.post("/register",async (req,res) => {
 
@@ -52,6 +61,108 @@ userRouter.post("/login",async (req,res) => {
         res.status(error.code).json(error.err);
     }
 
+});
+
+userRouter.post("/follow/",async (req,res)=>{
+    let follow_form = req.body;
+
+    try {
+
+        let user_info = {
+            name:req.headers.user,
+            token:req.headers.token
+        }
+
+        let response = await ctrl.Follow(user_info,follow_form);
+
+        if(response){
+            res.json(response);
+        }
+        
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+});
+
+userRouter.delete("/follow/",async (req,res)=>{
+    let follow_form = req.body;
+
+    try {
+
+        let user_info = {
+            name:req.headers.user,
+            token:req.headers.token
+        }
+
+        let response = await ctrl.UnFollow(user_info,follow_form);
+
+        if(response){
+            res.json(response);
+        }
+        
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+});
+
+userRouter.get("/follow/",async (req,res)=>{
+
+    try {
+
+        let user_info = {
+            name:req.headers.user,
+            token:req.headers.token
+        }
+
+        let response = await ctrl.GetFollowedUser(user_info);
+
+        if(response){
+            res.json(response);
+        }
+        
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+});
+
+userRouter.get("/posts/:id",async (req,res) => {
+    if(req.params.id){
+        let find_form = {
+            id : req.params.id
+        };
+
+        try {
+            let response = await ctrl.GetUserPost(find_form);
+
+            res.json(response);
+        } catch (error) {
+            res.status(error.code).json(error.err);
+        }
+
+        
+    }else{
+        res.status(400).json("No parameters");
+    }
+});
+
+userRouter.get("/i/:id",async (req,res) => {
+    if(req.params.id){
+        let find_form = {
+            id : req.params.id
+        };
+
+        try {
+            let response = await ctrl.GetUser(find_form);
+
+            res.json(response);
+        } catch (error) {
+            res.status(error.code).json(error.err);
+        }
+
+        
+    }else{
+        res.status(400).json("No parameters");
+    }
 });
 
 
